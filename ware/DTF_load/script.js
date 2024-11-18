5866ff67b1886f6ab28f98044e3c604cf4a8f344
wanderer <56039464+wanderer-00@users.noreply.github.com>
2024-11-18 17:51:12 +0300

    add DTF load info

--- ware/DTF_load/script.js
+++ ware/DTF_load/script.js
@@ -1,8 +1,10 @@
 let cluster = document.getElementsByClassName("cluster")[0];
 
 for (i = 0; i < dtfLoad.length; i++) {
+    let progress = 100/dtfLoad[i][2][1]*dtfLoad[i][2][0];
+    console.log(progress);
     cluster.innerHTML += `
-        <div>
+        <div style="background: linear-gradient(to right, #343434 ${progress}%, transparent ${progress}%);">
             <a class="img"      href="img/${dtfLoad[i][0]}" style="background-image: url(img/${dtfLoad[i][0]})"></a>
             <div class="name"   >${dtfLoad[i][1]}</div>
             <div class="status" >${dtfLoad[i][2][0]}/${dtfLoad[i][2][1]}</div>

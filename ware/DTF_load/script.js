ecf5ad3dc857e41fda62f49ff89f5c5506ec2748
wanderer <56039464+wanderer-00@users.noreply.github.com>
2024-11-20 12:38:30 +0300

    -

--- ware/DTF_load/script.js
+++ ware/DTF_load/script.js
@@ -4,11 +4,15 @@ for (i = 0; i < dtfLoad.length; i++) {
     let progress = 100/dtfLoad[i][2][1]*dtfLoad[i][2][0];
     console.log(progress);
     cluster.innerHTML += `
-        <div>
+        <div class="cell">
             <a class="img"      href="img/${dtfLoad[i][0]}" style="background-image: url(img/${dtfLoad[i][0]})"></a>
             <div class="name"   >${dtfLoad[i][1]}</div>
             <div class="status" >${dtfLoad[i][2][0]}/${dtfLoad[i][2][1]}</div>
             <div class="time"   >${dtfLoad[i][3].toFixed(1)} ч.</div>
+        </div>
+        <div class="progress">
+            <div style="width: ${progress}%;"></div>
+            <b>прогресс ${progress.toFixed(0)}%</b>
         </div>
 `;
 };

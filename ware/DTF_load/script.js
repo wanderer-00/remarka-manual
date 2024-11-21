cbe7b5fbcf38cafe97b6890fc57060755af1f501
wanderer <56039464+wanderer-00@users.noreply.github.com>
2024-11-21 11:27:53 +0300

    Update script.js

--- ware/DTF_load/script.js
+++ ware/DTF_load/script.js
@@ -7,7 +7,7 @@ for (i = 0; i < dtfLoad.length; i++) {
         <div class="cell">
             <a class="img"      href="img/${dtfLoad[i][0]}" style="background-image: url(img/${dtfLoad[i][0]})"></a>
             <div class="name"   >${dtfLoad[i][1]}</div>
-            <div class="status" >${dtfLoad[i][2][0]} / ${dtfLoad[i][2][1]}</br>${status(dtfLoad[i][4])}</div>
+            <div class="status" >${dtfLoad[i][2][0]} / ${dtfLoad[i][2][1]}<hr>${status(dtfLoad[i][4])}</div>
             <div class="time"   >${dtfLoad[i][3].toFixed(1)} ч.</div>
         </div>
         <div class="progress">
